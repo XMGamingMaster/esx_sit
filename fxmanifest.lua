5b15d8d7c28e553469fad6e5c325a23c08acb9f3
Steven Modl <steven@fam-modl.de>
2022-08-24 05:09:21 +0200

    bump version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'renaiku, indilo53, CmJustice and GamingMaster#4361'
 description 'This script allow you to sit almost everywhere'
-version '1.0.2'
+version '1.1.0'
 
 shared_script 'config.lua'
 client_scripts {
